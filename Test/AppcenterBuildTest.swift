4b90fb5cefcdc2c90672a64024f086d120ba7446
Thong Hao Yi <hy.thong@elken.com>
2019-07-02 10:29:25 +0800

    Appstore research

---
+++ Test/AppcenterBuildTest.swift
@@ -0,0 +1,30 @@
+//
+//  AppcenterBuildTest.swift
+//  AppCenterBuild
+//
+//  Created by Thong Hao Yi on 02/07/2019.
+//  Copyright © 2019 Thong Hao Yi. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class AppcenterBuildTest: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        XCTAssertTrue("this is a string".count > 0)
+    }
+}
